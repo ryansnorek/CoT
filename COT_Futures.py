5e92ae38750c674fc5eb15d00743c1aafcd41b50
ryansnorek <32182282+ryansnorek@users.noreply.github.com>
2020-12-01 17:17:40 -0800

    Update COT_Futures.py

--- COT_Futures.py
+++ COT_Futures.py
@@ -4,6 +4,7 @@ CoT_dis_url = 'https://www.cftc.gov/dea/newcot/f_disagg.txt'
 
 # Skeletons for lists
 CoT_list = []
+CoT_top = []
 title, date, open_interest = [], [], []
 am_long, am_long_change = [], []
 am_short, am_short_change = [], []
@@ -12,12 +13,27 @@ lf_long, lf_short, lf_spreading = [], [], []
 lf_long_change, lf_short_change, lf_spreading_change = [], [], []
 
 def financials():
-    # Retrieve CoT Financial Futures from URL
+    # Retrieve CoT Financials Futures from URL
     retrieve(CoT_fin_url,'cot_fin.txt')
 
     # Open CoT Financial Futures file and organize data
     with open("cot_fin.txt", 'r') as txtFile:
-        organize_data(txtFile)
+        organizeData(txtFile)
+
+    CoT_top.append(CoT_list[4]) # Euro
+    CoT_top.append(CoT_list[18]) # S&P Energy
+    CoT_top.append(CoT_list[19]) # S&P 500
+    CoT_top.append(CoT_list[20]) # S&P Financial
+    CoT_top.append(CoT_list[25]) # Nasdaq
+    CoT_top.append(CoT_list[27]) # Russel
+    CoT_top.append(CoT_list[32]) # Emerging
+    CoT_top.append(CoT_list[35]) # Bonds
+    CoT_top.append(CoT_list[37]) # 2YR
+    CoT_top.append(CoT_list[38]) # 10YR
+    CoT_top.append(CoT_list[46]) # Bitcoin
+    CoT_top.append(CoT_list[47]) # DXY
+    CoT_top.append(CoT_list[48]) # Vix
+    return CoT_top
 
 def disaggregated():
     # Retrieve CoT Disaggregated Futures from URL
@@ -25,147 +41,146 @@ def disaggregated():
 
     # Open CoT Disaggregated Futures file and organize data
     with open("cot_dis.txt", 'r') as txtFile:
-        organize_data(txtFile)
+        organizeData(txtFile)
 
-def organize_data(txtFile):
+    CoT_top.append(CoT_list[75]) # Natural Gas
+    CoT_top.append(CoT_list[193]) # WTI
+    CoT_top.append(CoT_list[201]) # Platinum
+    CoT_top.append(CoT_list[204]) # Silver
+    CoT_top.append(CoT_list[205]) # Copper
+    CoT_top.append(CoT_list[206]) # Gold
+    CoT_top.append(CoT_list[207]) # Gasoline
+    return CoT_top
+
+def organizeData(txtFile):
     for line in txtFile:
-        # Add each section to the CoT List
         stripped_line = line.strip()
         CoT_list.append(stripped_line)
+    return CoT_list
 
-    # Loop through the CoT List and isolate each item by comma 
-    for line in CoT_list:
+def parseFinData(data):
+    for line in data:
         line = line.split(',') 
+        # Title, Date, Open Interest
+        title.append(line[0]) 
+        date.append(line[2])
+        open_interest.append(line[7])
 
-        # Skip lines that have a comma in the main title (unsolved solution)
+        # Asset Managers / Institutions
+        am_long.append(line[11])
+        am_long_change.append(line[28])
+        am_short.append(line[12])
+        am_short_change.append(line[29])
+        am_spreading.append(line[13])   
+        am_spreading_change.append(line[30])
+
+        # Leveraged Funds
+        lf_long.append(line[14])
+        lf_long_change.append(line[31])
+        lf_short.append(line[15])
+        lf_short_change.append(line[32])
+        lf_spreading.append(line[16])
+        lf_spreading_change.append(line[33])
+
+def printFinData():
+    for i in range(len(CoT_top)):
+        print(title[i])
+        print("-------------------------")
+        print("Date:",date[i])
+        print("Open Interest:",open_interest[i])
+        print("-------------------------")
+        print("Asset Managers / Institutions")
+
+        print("\nLong:",am_long[i],am_long_change[i],"change")
+        print("Short:",am_short[i],am_short_change[i],"change")
+        print("Spreading:",am_spreading[i],am_spreading_change[i],"change")
+        print("-------------------------")
+        print("Leveraged Funds")
+
+        print("\nLong:",lf_long[i],lf_long_change[i],"change")
+        print("Short:",lf_short[i],lf_short_change[i],"change")
+        print("Spreading:",lf_spreading[i],lf_spreading_change[i],"change")
+        print("-------------------------")
+
+def parseDisData(data):
+    for line in data:
+        line = line.split(',')
+
+        # WTI Crude Oil has 2 commas, shift over by 1
+        if len(line[0]) < 12:
+            # Title, Date, Open Interest
+            title.append(line[0]) 
+            date.append(line[3])
+            open_interest.append(line[8])
+
+             # Leveraged Funds / Swap Dealers
+            lf_long.append(line[11])
+            lf_long_change.append(line[59])
+            lf_short.append(line[12])
+            lf_short_change.append(line[60])
+            lf_spreading.append(line[13])
+            lf_spreading_change.append(line[61])
+
+            # Asset Managers / Institutions
+            am_long.append(line[14])
+            am_long_change.append(line[62])
+            am_short.append(line[15])
+            am_short_change.append(line[63])
+            am_spreading.append(line[16])   
+            am_spreading_change.append(line[64])
+
         if len(line[0]) > 12:
             # Title, Date, Open Interest
             title.append(line[0]) 
             date.append(line[2])
             open_interest.append(line[7])
 
+            # Leveraged Funds / Swap Dealers
+            lf_long.append(line[10])
+            lf_long_change.append(line[58])
+            lf_short.append(line[11])
+            lf_short_change.append(line[59])
+            lf_spreading.append(line[12])
+            lf_spreading_change.append(line[60])
+
             # Asset Managers / Institutions
-            am_long.append(line[11])
-            am_long_change.append(line[28])
-            am_short.append(line[12])
-            am_short_change.append(line[29])
-            am_spreading.append(line[13])   
-            am_spreading_change.append(line[30])
+            am_long.append(line[13])
+            am_long_change.append(line[61])
+            am_short.append(line[14])
+            am_short_change.append(line[62])
+            am_spreading.append(line[15])   
+            am_spreading_change.append(line[63])
 
-            # Leveraged Funds
-            lf_long.append(line[14])
-            lf_long_change.append(line[31])
-            lf_short.append(line[15])
-            lf_short_change.append(line[32])
-            lf_spreading.append(line[16])
-            lf_spreading_change.append(line[33])
-
-    # Print a numbered list of title selections
-    i = 0
-    for selection in title:
-        print(i, title[i])
-        i += 1
-
-    # Get input from user and format data 
-    print("-------------------------")
-    inp = int(input("Type a number from the list or hit enter to quit program: "))
-    while inp != '':
-        print(title[inp])
+def printDisData():
+    for i in range(len(CoT_top)):
+        print(title[i])
         print("-------------------------")
-        print("Date:",date[inp])
-        print("Open Interest:",open_interest[inp])
+        print("Date:",date[i])
+        print("Open Interest:",open_interest[i])
+        print("-------------------------")
+        print("Leveraged Funds / Swap Dealers")
+
+        print("\nLong:",lf_long[i],lf_long_change[i],"change")
+        print("Short:",lf_short[i],lf_short_change[i],"change")
+        print("Spreading:",lf_spreading[i],lf_spreading_change[i],"change")
         print("-------------------------")
         print("Asset Managers / Institutions")
-        
-        print("\nLong:",am_long[inp],am_long_change[inp],"change")
-        print("Short:",am_short[inp],am_short_change[inp],"change")
-        print("Spreading:",am_spreading[inp],am_spreading_change[inp],"change")
+
+        print("\nLong:",am_long[i],am_long_change[i],"change")
+        print("Short:",am_short[i],am_short_change[i],"change")
+        print("Spreading:",am_spreading[i],am_spreading_change[i],"change")
         print("-------------------------")
-        print("Leveraged Funds")
-        
-        print("\nLong:",lf_long[inp],lf_long_change[inp],"change")
-        print("Short:",lf_short[inp],lf_short_change[inp],"change")
-        print("Spreading:",lf_spreading[inp],lf_spreading_change[inp],"change")
-        print("-------------------------")
-        inp = int(input("Type a number from the list or hit enter to quit program: "))
 
+def runProgram():
+    print('\n1. Financials')
+    print('2. Disaggregated')
+    selection = int(input("\nChoose category: "))
+    if selection == 1:
+        parseFinData(financials())
+        printFinData()
+    elif selection == 2:
+        parseDisData(disaggregated())
+        printDisData()
 
-# Starting Menu
-print("1. Financials")
-print("2. Disaggregated")
-
-# Ask user to select a CoT report
-select_report = int(input("Select a CoT futures report to view "))
-if select_report == 1:
-    financials()
-elif select_report == 2:
-    disaggregated()
-
-
-            
-
-        
-
-
-        
-
-        
-            
-   
-
-        
-           
-            
-
-       
-           
-          
-            
-       
-            
-       
-
-       
-
-        
-    
-    
-    
-   
-
-
-
-
-
-            
-
-        
-
-
-        
-
-        
-            
-   
-
-        
-           
-            
-
-       
-           
-          
-            
-       
-            
-       
-
-       
-
-        
-    
-    
-    
-   
-
+runProgram()
 
